71f1e3b3b6c04ff48b5c79ebfbc05707767c5c7a
 NIKA <nika@MacBook-Pro-NIKA.local>
2023-01-28 13:12:05 +0400

    added an authentication and registration

--- training_room/urls.py
+++ training_room/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.main, name="home"),
     path("gym", views.gym, name="gym"),
+    path("logout/", views.logoutUser, name="logout"),
+    path("signup/", views.registration, name="registration"),
 ]
